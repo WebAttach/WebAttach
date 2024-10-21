82d9a9dcea087747578194489fb71788cb031100
Web Attach <110483060+WebAttach@users.noreply.github.com>
2024-10-21 13:35:55 -0700

    Update webattach3.js

--- webattach3.js
+++ webattach3.js
@@ -73,7 +73,7 @@ let currentIndex = 0;
 
 // Function to change the background color with smooth transitions
 function changeColor() {
-   
+   alert("Line 76")
   // Select the ancestor <td> with the 'clsBBarRaised' class (outer strip for top and bottom)
   const outerTdElement = document.querySelector("td.clsBBarRaised");
 
